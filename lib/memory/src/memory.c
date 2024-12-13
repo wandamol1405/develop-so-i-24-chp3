00e195f94ef183566fe2cbaca7f8df34888418fd
Wanda Molina <wanda.molina@mi.unc.edu.ar>
2024-12-13 14:37:26 -0300

    Fix split_block

--- lib/memory/src/memory.c
+++ lib/memory/src/memory.c
@@ -84,7 +84,7 @@ t_block find_block(t_block *last, size_t size) {
 }
 
 void split_block(t_block b, size_t s) {
-  if (b->size <= s + BLOCK_SIZE) {
+  if (!valid_addr(b) && b->size <= s + BLOCK_SIZE) {
     return;
   }
 
