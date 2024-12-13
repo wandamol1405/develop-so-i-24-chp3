614128f229f42508748954430d64be2f28023d1c
Wanda Molina <wanda.molina@mi.unc.edu.ar>
2024-12-13 16:43:27 -0300

    Fix split_block

--- lib/memory/src/memory.c
+++ lib/memory/src/memory.c
@@ -74,16 +74,10 @@ void split_block(t_block b, size_t s) {
   if (b->size <= s + BLOCK_SIZE) {
     return;
   }
-  // Comprobación de seguridad para asegurarse de que 'b->data + s' está dentro
-  // de los límites del bloque
-  if ((b->data + s) >= (b->data + b->size)) {
-    printf("Error: 'new' apunta fuera del bloque original\n");
-    return;
-  }
 
   t_block new;
   new = (t_block)(b->data + s);
-  new->size = b->size - s - BLOCK_SIZE;
+  new->size = (b->size - s) - BLOCK_SIZE;
   new->next = b->next;
   new->prev = b;
   new->free = 1;
